4bdb648d9480832062b839304d27c0da25d0b94b
alicifes <635023574@qq.com>
2023-06-29 10:42:51 +0800

    auto push at 2023-06-29 10:42:51

--- demo03.ts
+++ demo03.ts
@@ -10,26 +10,47 @@
 // type几乎什么时候都可以使用 alias别名
 
 //type表示的是类型，并不是具体的值
-type FalseLike = 0 | '' | null | undefined | false
+type FalseLike = 0 | "" | null | undefined | false;
 type FnWithProp = {
-    prop:string,
-    (a:number,b:string): void //对象内部使用的是函数,这里不需要使用() => {}来声明
-}
+  prop: string;
+  (a: number, b: string): void; //对象内部使用的是函数,这里不需要使用() => {}来声明
+};
 
-const lsa:FnWithProp = (a:number,b:string) => {
-    return a+b
-}
-lsa.prop = 'sad'
+const lsa: FnWithProp = (a: number, b: string) => {
+  return a + b;
+};
+lsa.prop = "sad";
 //在react中使用较多。props来实现默认的定义
 
 //interface 接口:面向对象oop 功能。函数。属性
 //描述对象的属性 declare,以下两者等价
-type A1 = Array<string>
-interface A2 extends Array<string> {
-}
+type A1 = Array<string>;
+interface A2 extends Array<string> {}
 //一般来说 type用来描述基本的数据类型7种,而对于其他的数据类型的话Array... 需要使用interface来实现
 //interface只描述对象 type可以描述所有的数据    type其实只是别名 而interface是类型声明(真名)
 //sp type不可以赋值 interface自动合并，可扩展
 interface aaa extends Array<string> {
-    sscc:'saw',
+  sscc: "saw";
 }
+//interface的扩展性远远大于type的扩展性
+// declare globals {
+//   interface String {
+//     parseZero(x: string): void;
+//   }
+// }
+//对外interface 对内type防止代码分散
+//对于继承来说，不一定是需要interface，type也可以实现继承,使用and来实现
+type A = {
+  aaa: "123";
+};
+
+type B = {
+  bbb: "12312";
+} & A;
+
+const c: B = {
+  aaa: "123",
+  bbb: "12312",
+};
+
+//返回void ，但是可以传递任何类型，只是值不能用
